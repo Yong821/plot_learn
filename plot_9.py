82dc72c016dae714ad859dc384a0b6d351965853
aurora <aurora__fly@126.com>
2022-09-10 08:33:44 +0800

    读取图像显示

--- plot_9.py
+++ plot_9.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n = 81
-a = np.random.uniform(0, 1, n).reshape(9, 9)
+a = np.random.uniform(0, 1, n).reshape(9, 9)    # 81 values are obtained from a uniform distribution, and reshape the array
 
-plt.imshow(a, interpolation='nearest', cmap='bone', origin='upper')
+plt.imshow(a, interpolation='nearest', cmap='bone', origin='upper')     # read them
 
-plt.xticks(())
-plt.yticks(())
-plt.colorbar(shrink=0.9)
+plt.xticks(())              # let x-axis disappear
+plt.yticks(())              # ...
+plt.colorbar(shrink=0.9)    # color-bar:right the img, and let its height is 0.9 times the height of img
 plt.show()
